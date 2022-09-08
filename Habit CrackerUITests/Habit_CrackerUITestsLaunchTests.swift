7ffa6471bd1f69ec5338ab2a5b0f48506f27a0d4
Joshua Dadson <joshua.dadson@ibm.com>
2022-09-08 17:11:26 -0500

    Initial Commit

---
+++ Habit CrackerUITests/Habit_CrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Habit_CrackerUITestsLaunchTests.swift
+//  Habit CrackerUITests
+//
+//  Created by Joshua Dadson on 9/8/22.
+//
+
+import XCTest
+
+class Habit_CrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
